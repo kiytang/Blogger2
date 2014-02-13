9c028c9a09432b12b87b13da5031e810a8ea6fa5
Kiy Hong Tang <kiyhong@gmail.com>
2014-02-13 17:50:01 +0000

    introduced relationships

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,4 @@
 class Article < ActiveRecord::Base
+	#article has many comments
+	has_many :comments
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ActiveRecord::Base
+  #comment relates to a single article
   belongs_to :article
 end
