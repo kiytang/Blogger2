b43ef72d908daa2ae3f270ce80f6d858a6fc3377
Kiy Hong Tang <kiyhong@gmail.com>
2014-02-24 09:43:15 +0000

    Tagging feature completed

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,2 +1,17 @@
 class TagsController < ApplicationController
+
+	def show
+		@tag = Tag.find(params[:id])		
+	end
+
+#create index action
+	def index
+		@tags = Tag.all
+	end
+
+	def destroy
+		@tag = Tag.find(params[:id])
+		@tag.destroy
+	end
+
 end
